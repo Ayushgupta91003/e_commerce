f777e268375c6106e325188fa8d842b8f5aefa4c
ayush <ayush91003@gmail.com>
2025-01-12 15:00:10 +0530

    api removed and starting backend

--- server/index.js
+++ server/index.js
@@ -1,28 +1,7 @@
-console.log("Hello, World!");
-
 const express = require('express');
 const PORT = 3000;
 
 const app = express();
-
-// creating an api
-// http://<youripaddress>/hello-world
-
-// create a get request giving a json response with the key of name and the value of your name.
-// {
-//     'name' : 'Ayush';
-// }
-
-app.get("/", (req,res)=>{
-    res.json({"name": "Rivaan"});
-})
-
-
-app.get("/hello-world", (req,res) => {
-    res.send("Hello World");
-})
-
-
 
 app.listen(PORT, () => {
     console.log(`connected at port ${PORT}`);
