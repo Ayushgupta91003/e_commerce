0a2022196c69c1a09be2b0a56f49e4d98a2c55fd
ayush <ayush91003@gmail.com>
2025-01-15 15:23:24 +0530

    Delete Product done

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-String uri = 'http://192.168.3.124:3000';
-// String uri = 'http://10.0.4.47:3000';
+// String uri = 'http://192.168.3.124:3000';     // my phone
+// String uri = 'http://192.168.2.124:3000'; // Paul
+String uri = 'http://10.0.4.47:3000';      // institute
 
 class GlobalVariables {
   // COLORS
--- lib/features/admin/screens/posts_screens.dart
+++ lib/features/admin/screens/posts_screens.dart
@@ -1,5 +1,9 @@
+import 'package:e_commerce/common/widgets/loader.dart';
 import 'package:e_commerce/constants/global_variables.dart';
+import 'package:e_commerce/features/account/widgets/single_product.dart';
 import 'package:e_commerce/features/admin/screens/add_product_screen.dart';
+import 'package:e_commerce/features/admin/services/admin_services.dart';
+import 'package:e_commerce/models/product.dart';
 import 'package:flutter/material.dart';
 
 class PostsScreen extends StatefulWidget {
@@ -10,24 +14,81 @@ class PostsScreen extends StatefulWidget {
 }
 
 class _PostsScreenState extends State<PostsScreen> {
+  List<Product>? products;
+  final AdminServices adminServices = AdminServices();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchAllProducts();
+  }
+
+  fetchAllProducts() async {
+    products = await adminServices.fetchAllProducts(context);
+    setState(() {});
+  }
+
+  void deleteProduct(Product product, int index) {
+    adminServices.deleteProduct(
+      context: context,
+      product: product,
+      onSuccess: () {
+        products!.removeAt(index);
+        setState(() {});
+      },
+    );
+  }
+
   void navigateToAddProduct() {
     Navigator.pushNamed(context, AddProductScreen.routeName);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.cyan[800],
-        onPressed: navigateToAddProduct,
-        child: const Icon(Icons.add),
-        tooltip: 'Add a Product',
-        shape: const CircleBorder(),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      body: Center(
-        child: Text('Products'),
-      ),
-    );
+    return products == null
+        ? const Loader()
+        : Scaffold(
+            body: GridView.builder(
+              itemCount: products!.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2),
+              itemBuilder: (context, index) {
+                final productData = products![index];
+                return Column(
+                  children: [
+                    SizedBox(
+                      height: 140,
+                      child: SingleProduct(image: productData.images[0]),
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            productData.name,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () => deleteProduct(productData, index),
+                          icon: const Icon(Icons.delete_outline),
+                        )
+                      ],
+                    ),
+                  ],
+                );
+              },
+            ),
+            floatingActionButton: FloatingActionButton(
+              backgroundColor: Colors.cyan[800],
+              onPressed: navigateToAddProduct,
+              child: const Icon(Icons.add),
+              tooltip: 'Add a Product',
+              shape: const CircleBorder(),
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+          );
   }
 }
